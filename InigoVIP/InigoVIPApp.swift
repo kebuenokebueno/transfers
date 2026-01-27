c478018408d5924dd52fb7a9579bdabd74b1d633
kebuenokebueno <kebuenokebueno@gmail.com>
2026-01-27 19:17:46 +0100

    Initial Commit

---
+++ InigoVIP/InigoVIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  InigoVIPApp.swift
+//  InigoVIP
+//
+//  Created by Inigo on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct InigoVIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InigoVIPTests/InigoVIPTests.swift
@@ -0,0 +1,17 @@
+//
+//  InigoVIPTests.swift
+//  InigoVIPTests
+//
+//  Created by Inigo on 27/1/26.
+//
+
+import Testing
+@testable import InigoVIP
+
+struct InigoVIPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
