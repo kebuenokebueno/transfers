8effbe0dbea80b5418b55e57f1b6d5e4133e8ff0
kebuenokebueno <kebuenokebueno@gmail.com>
2026-02-06 12:01:18 +0100

    chore: move NoteViewModel to separate file

--- InigoVIP/Models/NoteScene.swift
+++ InigoVIP/Models/NoteScene.swift
@@ -7,15 +7,6 @@
 
 import Foundation
 
-struct NoteViewModel: Identifiable {
-    let id: String
-    let amount: String
-    let description: String
-    let date: String
-    let category: String
-    let isPositive: Bool
-    let syncStatus: String
-}
 
 enum NoteScene {
     
---
+++ InigoVIP/Models/NoteViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  InigoVIP
+//
+//  Created by Inigo on 6/2/26.
+//
+
+import Foundation
+
+
+struct NoteViewModel: Identifiable {
+    let id: String
+    let amount: String
+    let description: String
+    let date: String
+    let category: String
+    let isPositive: Bool
+    let syncStatus: String
+}
